4889a73b0c7755035c23cc6b4ddb9d65225752cc
José Pérez Negro <peresnegro@hotmail.com>
2020-04-14 00:11:46 +0100

    Updates

---
+++ precache-manifest.4896491bbfec4c42995996f98f7b33b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36364dba9b68cf6f383e1f22ceca8491",
+    "url": "/corona-info/index.html"
+  },
+  {
+    "revision": "2c8d934cd62b6c09f827",
+    "url": "/corona-info/static/css/main.d54cc640.chunk.css"
+  },
+  {
+    "revision": "2d7385845b9b5a6b215c",
+    "url": "/corona-info/static/js/2.321594a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/corona-info/static/js/2.321594a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c8d934cd62b6c09f827",
+    "url": "/corona-info/static/js/main.a2368fdf.chunk.js"
+  },
+  {
+    "revision": "fe4f0c7b3f8729a27a46",
+    "url": "/corona-info/static/js/runtime-main.8d0b7d5a.js"
+  }
+]);
